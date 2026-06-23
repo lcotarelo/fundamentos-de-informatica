898bf2d0f1dce2dc2d40eab330d05a9b5f2a62ef
Leandro Cotarelo <lcotarelo@uade.edu.ar>
2026-06-23 14:17:53 -0300

    TODO remover espaciado de reporte

--- Gestor de camiones.py
+++ Gestor de camiones.py
@@ -15,28 +15,54 @@ INGRESAR_DISTANCIA_RECORRIDA = "Por favor, ingresar la distancia recorrida del c
 SEPARADOR = "---------------------------------------------------------------------------------------------"
 INGRESAR_CARGA_TRANSPORTADA = "Por favor, ingresar la carga transportada del camion en toneladas: "
 
-# def printTiempoCentrado(dias,horas,espacioMaximo):
-#   if dias==0 and horas==0:
-#     printCentradoNumText(0,HORAS,espacioMaximo)
-#     return
-#   if dias==0:
-#     printCentradoNumText(horas,HORAS,espacioMaximo)
-#     return
-#   if horas==0:
-#     printCentradoNumText(dias,DIAS,espacioMaximo)
-#     return
-#   largoTotal=contarDigitos(dias)+contarDigitos(horas)+3
-#   if largoTotal>=espacioMaximo:
-#     print(dias,end=STRING_VACIO)
-#     print(DIAS,horas,end=STRING_VACIO)
-#     print(HORAS,end=STRING_VACIO)
-#     return
-#   espaciosBlanco=espacioMaximo-largoTotal
-#   printNEspacios(espaciosBlanco//2)
-#   print(dias,end=STRING_VACIO)
-#   print(DIAS,horas,end=STRING_VACIO)
-#   print(HORAS,end=STRING_VACIO)
-#   printNEspacios(espaciosBlanco-espaciosBlanco//2)
+def contarDigitos(numero):
+  contador=0
+  if numero<0:
+    numero=numero*-1
+    contador=1  
+  while numero>0:
+    contador+=1
+    numero=numero//10
+  return contador
+
+def printNEspacios(cantidad):
+  for i in range(cantidad):
+    print(ESPACIO,end=STRING_VACIO)
+
+def printCentradoNumText(numero,texto,espacioMaximo):
+  largoTotal=contarDigitos(numero)+len(texto)
+  if largoTotal>=espacioMaximo:
+    print(numero,end=STRING_VACIO )
+    print(texto,end=STRING_VACIO)
+    return
+  espaciosBlanco=espacioMaximo-largoTotal
+  printNEspacios(espaciosBlanco//2)
+  print(numero,end=STRING_VACIO)
+  print(texto,end=STRING_VACIO)
+  printNEspacios(espaciosBlanco-espaciosBlanco//2)
+
+def printTiempoCentrado(dias,horas,espacioMaximo):
+  if dias==0 and horas==0:
+    printCentradoNumText(0,HORAS,espacioMaximo)
+    return
+  if dias==0:
+    printCentradoNumText(horas,HORAS,espacioMaximo)
+    return
+  if horas==0:
+    printCentradoNumText(dias,DIAS,espacioMaximo)
+    return
+  largoTotal=contarDigitos(dias)+contarDigitos(horas)+3
+  if largoTotal>=espacioMaximo:
+    print(dias,end=STRING_VACIO)
+    print(DIAS,horas,end=STRING_VACIO)
+    print(HORAS,end=STRING_VACIO)
+    return
+  espaciosBlanco=espacioMaximo-largoTotal
+  printNEspacios(espaciosBlanco//2)
+  print(dias,end=STRING_VACIO)
+  print(DIAS,horas,end=STRING_VACIO)
+  print(HORAS,end=STRING_VACIO)
+  printNEspacios(espaciosBlanco-espaciosBlanco//2)
 
 def intercambiarElementos(lista, posicion):
     aux = lista[posicion + 1]
@@ -59,8 +85,7 @@ def funcPromedio(tieDeUnCamion,cantidadDeViajesDeUnCamion):
   promedio=tieDeUnCamion//cantidadDeViajesDeUnCamion
   return promedio//24,promedio%24   
     
-def listar(idCamion,tiempoDeUsoDeCamion,distanciaTotalRecorrida,cargaCamion,cantidadDeViajes,idIngresado,
-           tiempoIngresado,distanciaIngresada,cargaIngresada):
+def listar(idCamion,tiempoDeUsoDeCamion,distanciaTotalRecorrida,cargaCamion,cantidadDeViajes,idIngresado,tiempoIngresado,distanciaIngresada,cargaIngresada):
   for i in range(len(idCamion)):
     if idIngresado==idCamion[i]:
       tiempoDeUsoDeCamion[i]=tiempoDeUsoDeCamion[i]+tiempoIngresado
@@ -84,15 +109,12 @@ def confirmarIngreso(idIngresado, tiempoIngresado, distanciaIngresada, cargaIngr
     print("Carga:", cargaIngresada)
     agrega = input("Ingrese S para aceptar u otra tecla para descartar el ingreso: ")
     if agrega == "S" or agrega == "SI" or agrega == "s" or agrega == "si" or agrega == "sI" or agrega == "Si":
-      listaDeCamiones = listar(idCamion, tiempoDeUsoDeCamion, distanciaTotalRecorrida, cargaCamion, 
-                               cantidadDeViajes, idIngresado, tiempoIngresado, distanciaIngresada, cargaIngresada)    
+      listaDeCamiones = listar(idCamion, tiempoDeUsoDeCamion, distanciaTotalRecorrida, cargaCamion, cantidadDeViajes, idIngresado, tiempoIngresado, distanciaIngresada, cargaIngresada)    
       print("El reporte actual fue agregado.")
     else:
       print("El reporte actual no ha sido agregado.")
 
-def imprimirListado(printCentradoNumText, printTiempoCentrado, funcPromedio, STRING_VACIO, 
-                    KILOMETRAJE_MINIMO_PARA_REVISION_TECNICA, KILOMETROS, TONELADAS, ANCHO_DE_COLUMNA, MENSAJE_REVISION_MECANICA, 
-                    idCamion, tiempoDeUsoDeCamion, distanciaTotalRecorrida, cargaCamion, cantidadDeViajes):
+def imprimirListado(printCentradoNumText, printTiempoCentrado, funcPromedio, STRING_VACIO, KILOMETRAJE_MINIMO_PARA_REVISION_TECNICA, KILOMETROS, TONELADAS, ANCHO_DE_COLUMNA, MENSAJE_REVISION_MECANICA, idCamion, tiempoDeUsoDeCamion, distanciaTotalRecorrida, cargaCamion, cantidadDeViajes):
     print(ENCABEZADO_DE_LISTADO)
     for i in range(len(idCamion)):
       printCentradoNumText(idCamion[i],STRING_VACIO,ANCHO_DE_COLUMNA)
